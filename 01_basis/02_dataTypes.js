bd9dc4dbef3d0a2e1cc2a158a898e2ea45a74228
Raushan Kumar <92165794+Raushan75@users.noreply.github.com>
2025-04-02 12:59:03 +0000

    dataType and conversion are done

---
+++ 01_basis/02_dataTypes.js
@@ -0,0 +1,30 @@
+"use strict"; //treat all js code as newer version
+
+// alert("hello") // we are using node js, not browser
+
+console.log(3+3) // code readability should be high
+
+console.log("raushan")
+
+let name ="raushan"
+
+let age =21
+
+let isLoggedIn = false
+
+let state;
+
+// number => 2 to power 53
+// bigInt 
+// string => ""
+// boolean => true/false
+// undefined =>
+// null => standalone value 
+// symbol => unique
+
+// object
+
+console.log(typeof "raushan")
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
+
---
+++ 01_basis/03_converstion.js
@@ -0,0 +1,29 @@
+let score ="6";
+
+console.log(typeof score)
+
+let valueInNumber = Number(score);
+
+console.log(typeof valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => true; false => 0
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+
+console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "raushan" => true
+
+let someNumber = 33
+
+let strinNumber = String(someNumber);
+
+console.log(strinNumber)
+
+console.log(typeof strinNumber)
