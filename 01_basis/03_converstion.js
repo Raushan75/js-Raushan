03757cc6d5a8b2b76bd7a3f2470e162cb3b533f0
Raushan Kumar <92165794+Raushan75@users.noreply.github.com>
2025-04-03 17:39:25 +0000

    conversion is done

--- 01_basis/03_converstion.js
+++ 01_basis/03_converstion.js
@@ -1,29 +1,55 @@
-let score ="6";
+// let score ="6";
 
-console.log(typeof score)
+// console.log(typeof score)
 
-let valueInNumber = Number(score);
+// let valueInNumber = Number(score);
 
-console.log(typeof valueInNumber);
+// console.log(typeof valueInNumber);
 
 
 // "33" => 33
 // "33abc" => NaN
-// true => true; false => 0
-let isLoggedIn = 1
+// // true => true; false => 0
+// let isLoggedIn = 1
 
-let booleanIsLoggedIn = Boolean(isLoggedIn);
+// let booleanIsLoggedIn = Boolean(isLoggedIn);
 
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 // 1 => true; 0 => false
 // "" => false
 // "raushan" => true
 
-let someNumber = 33
+// let someNumber = 33
 
-let strinNumber = String(someNumber);
+// let strinNumber = String(someNumber);
 
-console.log(strinNumber)
+// console.log(strinNumber)
 
-console.log(typeof strinNumber)
+// console.log(typeof strinNumber)
+
+
+// ***************Operations*******************//
+
+// let value =3
+// let negValue = -value
+// console.log(negValue);
+
+// let str1 = "hello"
+// let str2 = "raushan"
+
+// let str3= str1 + str2
+// console.log(str3)
+
+// console.log(+true);
+// console.log(+"");
+
+// let num1,num2,num3
+
+num1 = num2 = num3 = 2+2
+
+let gameCounter =100
+
+++gameCounter
+
+console.log(gameCounter);
