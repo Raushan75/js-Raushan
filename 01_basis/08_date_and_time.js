df223ff548e98dcea9b929c12864891f08964eee
Raushan Kumar <92165794+Raushan75@users.noreply.github.com>
2025-04-06 10:45:57 +0000

    date and time completed

---
+++ 01_basis/08_date_and_time.js
@@ -0,0 +1,10 @@
+let myDate = new Date();
+console.log(myDate.toString());
+
+console.log(myDate.toDateString());
+console.log(myDate.toISOString());
+console.log(myDate.toLocaleDateString());
+
+
+let myCreateDate = new Date( 2025,0,29)
+console.log(myCreateDate)
